e1b9551bf8b7ed1500a9ea75bbc25c994ec0c413
lamportunit <lamportunit@users.noreply.github.com>
2026-03-14 18:01:00 +0700

    fix: module exports

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -30,3 +30,4 @@ pub fn checked_sub(a: u64, b: u64) -> Result<u64, LamportError> { a.checked_sub(
 // updated: 2026-03-06 10:19
 // updated: 2026-03-09 16:43
 // updated: 2026-03-11 15:09
+// updated: 2026-03-14 18:01
